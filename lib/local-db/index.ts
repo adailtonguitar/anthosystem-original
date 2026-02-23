76c2780d088fe92a290c71b6ed6f9bd8f8532169
adailtonguitar <adailtonguitar@gmail.com>
2026-02-23 09:09:00 -0300

    mover arquivos para src/

--- lib/local-db/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Local database barrel export.
- * Usage: import { DataLayer, initLocalDB } from "@/lib/local-db";
- */
-export { initLocalDB, getDB, closeLocalDB } from "./connection";
-export { DataLayer } from "./data-layer";
-export type { QueryResult, SingleResult } from "./data-layer";
-export { DB_NAME } from "./schema";
